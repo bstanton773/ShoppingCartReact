b7567e743b907b43879825d9df3d983f64a85454
Brian <bstanton773@gmail.com>
2021-08-30 15:16:08 -0500

    Login page

--- src/App.js
+++ src/App.js
@@ -2,34 +2,62 @@ import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav'
 import Home from './views/Home';
+import Login from './views/Login';
 import Register from './views/Register';
 
 export default class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      isLoggedIn: false,
-      products: []
+    constructor(props){
+        super(props);
+        this.state = {
+            isLoggedIn: localStorage.getItem('token') !== null,
+            products: []
+        }
     }
-  }
 
-  componentDidMount(){
-    console.log('Hello')
-  }
+    componentDidMount(){
+        console.log('Hello')
+    }
+
+    logUserIn = (e) => {
+        e.preventDefault();
+        console.log(e)
+        const username = e.target.username.value;
+        const password = e.target.password.value;
+
+        let myHeaders = new Headers();
+        const credentials = btoa(`${username}:${password}`);
+        myHeaders.append('Authorization', 'Basic ' + credentials);
+
+        fetch('http://cart-api-66.herokuapp.com/tokens', {
+            method: 'POST',
+            headers: myHeaders
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            this.setState({
+                isLoggedIn: true
+             })
+             localStorage.setItem('token', data.token)
+            }
+        )
+        .catch(err => console.error(err))
+    }
 
   
-  render() {
-    return (
-      <div className='bg-secondary'>
-        <Nav isLoggedIn={this.state.isLoggedIn} />
-        <div className='container bg-white border-start border-end vh-100'>
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/register' component={Register} />
-          </Switch>
+    render() {
+        return (
+        <div className='bg-secondary'>
+            <Nav isLoggedIn={this.state.isLoggedIn} />
+            <div className='container bg-white border-start border-end vh-100'>
+            <Switch>
+                <Route exact path='/' component={Home} />
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' render={() => <Login logUserIn={this.logUserIn} />}/>
+            </Switch>
+            </div>
         </div>
-      </div>
-    )
-  }
+        )
+    }
 }
 
---
+++ src/views/Login.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export default class Login extends Component {
+
+
+    render() {
+        return (
+            <div>
+                <h4 className='text-center'>Create Your Kekambas Account Here:</h4>
+                <form className='mx-5' onSubmit={this.props.logUserIn}>
+                    <div className='form-group'>
+                        <fieldset>
+                            <label htmlFor='username'>Username</label>
+                            <input type='text' className='form-control' name='username' placeholder='Username' />
+                            <label htmlFor='password'>Password</label>
+                            <input type='password' className='form-control' name='password' placeholder='Password' />
+                        </fieldset>
+                        <input type='submit' className='btn btn-outline-primary mt-3 w-100' value='Log In'></input>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
